3402ea5ae1b0a405374414f6ea97b669b070b148
Sina Ghaffari <sinaghafari.dev@gmail.com>
2024-06-26 00:12:00 +0330

    Create Accounting Project

---
+++ Accounting/Accounting.DataLayer/Repositories/ICustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.DataLayer.Repositories
+{
+    internal interface ICustomerRepository
+    {
+        List<Customers> GetAllCustomers();
+        Customers GetCustomerById(int customerId);
+        bool InsertCustomer(Customers customer);
+        bool UpdateCustomer(Customers customer);
+        bool DeleteCustomer(Customers customer);
+        bool DeleteCustomer(int customerId);
+        void Save();
+    }
+}
---
+++ Accounting/Accounting.DataLayer/Services/CustomerRepository.cs
@@ -0,0 +1,47 @@
+﻿using Accounting.DataLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.DataLayer.Services
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public bool DeleteCustomer(Customers customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteCustomer(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customers> GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customers GetCustomerById(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InsertCustomer(Customers customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateCustomer(Customers customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
